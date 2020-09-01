fbb8e1bb96a364ba122a02f7444b784a3196ba66
gosaigosai <shubhamgirigosai@gmail.com>
2020-09-01 18:10:02 +0530

    hauntRadio

---
+++ GVSGB/Assets/Scripts/Mingames/HauntRadio/circleSlider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+
+public class circleSlider : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] Transform handle;
+    [SerializeField] public float rotateSpeed;
+    [SerializeField] public bool isPressed;
+    [SerializeField] public AudioSource audioSource;
+    Transform angle;
+
+    public void Start()
+    {
+       
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+
+    public void PlayAudio()
+    {
+        audioSource.Play();
+    }
+
+    public void Update()
+    {
+        if (isPressed)
+        {
+            handle.transform.Rotate(Vector3.forward * rotateSpeed);
+            audioSource = GetComponent<AudioSource>();
+            audioSource.Play(0);
+        }
+        else
+        {
+            audioSource.Pause();
+        }
+    }
+}
---
+++ GVSGB/Assets/Scripts/Mingames/Incantation/hauntRadio.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+
+using UnityEngine;
+
+namespace GVSGB
+{
+    public class hauntRadio : MonoBehaviour
+    {
+        [SerializeField]
+        MingameBase minigame;
+
+        [SerializeField]
+        GameObject needle;
+        [SerializeField]
+        GameObject points;
+
+        [SerializeField]
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            minigame = new MingameBase();
+            
+            
+        }
+
+        
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (minigame.MyState == MiniGameState.NOTSTARTED)
+                {
+                    minigame.MyState = MiniGameState.INPROGRESS;
+                    
+                }
+            }
+            if (minigame.MyState == MiniGameState.INPROGRESS)
+            {
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    
+                }
+                //if (Input.GetKeyDown(KeyCode.Space))
+                //{
+                //    CreatePipes();
+
+                //}
+                
+            }
+        }
+
+       
+
+      
+    }
+}
